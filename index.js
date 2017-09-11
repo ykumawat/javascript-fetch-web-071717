c9a30f57b1f924d2ba2e3b1878af09d8a0610de4
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-09-11 15:46:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+
+fetch('https://api.github.com/user/repos').
+  then(res => res.json()).
+  then(json => console.log(json))
+
+  const token = 'TOKEN'
+  fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+  }).then(res => res.json()).then(json => console.log(json));
